b2328a3e6409ba60b44544e7a14827494da94f3c
Giovanna <giovannagomes5476@gmail.com>
2024-04-23 18:14:20 -0300

    atualização do conteúdo

---
+++ 01 dominando a linguagem de programação java/01 aprendendo a sintaxe java/aula-001-anatomia-das-classes/src/Exemplo003.java
@@ -0,0 +1,8 @@
+public class Exemplo003 {
+    public static void main (String [] args) {
+        String meuNome = "Giovanna";
+        int anoFabricacao = 2022;
+        boolean verdadeiro = true;
+        anoFabricacao = 2018;
+    }
+}
---
+++ 01 dominando a linguagem de programação java/01 aprendendo a sintaxe java/aula-001-anatomia-das-classes/src/Exemplo004.java
@@ -0,0 +1,12 @@
+public class Exemplo004 {
+    public static void main (String[] args) {
+        String primeiroNome = "Giovanna";
+        String segundoNome = "Rodrigues";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
