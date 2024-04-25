207f07cce7b23c0fb7b09ff02444b6a53b2af0ce
Giovanna <giovannagomes5476@gmail.com>
2024-04-25 18:31:36 -0300

    atualização do conteúdo

---
+++ 01 dominando a linguagem de programação java/01-aprendendo-a-sintaxe-java/src/edu/giovanna/aula02/Exemplo001.java
@@ -0,0 +1,12 @@
+package edu.giovanna.aula02;
+
+public class Exemplo001 {
+    public static void main (String [] args) {
+        byte idade = 24;
+        short ano = 2021;
+        int cep = 21070333;
+        long cpf = 98765432109L;
+        float pi = 3.14F;
+        double salario = 1275.33;
+    }
+}
---
+++ 01 dominando a linguagem de programação java/01-aprendendo-a-sintaxe-java/src/edu/giovanna/aula02/Exemplo002.java
@@ -0,0 +1,12 @@
+package edu.giovanna.aula02;
+
+public class Exemplo002 {
+    public static void main (String [] args) {
+        int numero = 5;
+        numero = 10;
+
+        System.out.println (numero);
+
+        final double VALOR_DE_PI = 3.14;
+    }
+}
